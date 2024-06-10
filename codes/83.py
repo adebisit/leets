b60fa89d19445c87a6f81c27688efe6c46de1b20
adebisit <adebisijosephh@gmail.com>
2024-06-10 23:17:33 +0100

    Day 10

--- codes/83.py
+++ codes/83.py
@@ -2,13 +2,14 @@ from typing import Optional
 from dsTypes.linkedList import ListNode
 
 def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+    dummy = ListNode()
+    current = dummy
+
     if head.next is None:
         return head
-    current = ListNode(val=head.val)
-    head = head.next
     while head is not None:
         if head.val != current.val:
             current.next = ListNode(head.val)
             current = current.next
         head = head.next
-    return current
+    return dummy.next
