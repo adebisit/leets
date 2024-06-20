f6afd95854916d64fa54a0c8aeb72673eaf08a82
adebisit <adebisijosephh@gmail.com>
2024-06-20 15:44:27 +0100

    Day 17

--- codes/121.py
+++ codes/121.py
@@ -1,11 +1,27 @@
 from typing import List
 
-def maxProfit(self, prices: List[int]) -> int:
+def maxProfit(prices: List[int]) -> int:
     n = len(prices)
-    max_profit = 0
-    for i in range(n):
-        for j in range(i + 1, n):
-            profit = prices[j] - prices[i]
-            if profit > max_profit:
-                max_profit = profit
-    return max_profit
+    if n <= 1:
+        return 0
+    minNoLeft = [prices[0]]
+    maxNoRight = [prices[-1]]
+    for i in range(1, n):
+        if prices[i] < minNoLeft[-1]:
+            minNoLeft.append(prices[i])
+        else:
+            minNoLeft.append(minNoLeft[-1])
+        
+        if prices[n - i - 1] > maxNoRight[-1]:
+            maxNoRight.append(prices[n - i - 1])
+        else:
+            maxNoRight.append(maxNoRight[-1])
+
+    max_profit = maxNoRight[1] - minNoLeft[0]
+    for i in range(1, n - 1):
+        if maxNoRight[n - i - 1] - minNoLeft[i] > max_profit:
+            max_profit = maxNoRight[n - i - 1] - minNoLeft[i]
+    return max_profit if max_profit > 0 else 0
+
+# print(maxProfit([7,1,5,3,6,4]))
+print(maxProfit([5]))
